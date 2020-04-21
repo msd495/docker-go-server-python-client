a20c1f286158914710fa8aaf613deca99fde0f0c
Ayush495 <msd495@gmail.com>
2020-04-21 22:12:18 +0530

    Add files via upload

--- server/main.go
+++ server/main.go
@@ -1,1 +1,16 @@
-
+package main
+import (
+ "fmt"
+ "net/http"
+ "os"
+)
+func main() {
+ var PORT string
+ if PORT = os.Getenv("PORT"); PORT == "" {
+  PORT = "3001"
+ }
+ http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+  fmt.Fprintf(w, "Hello World ")
+ })
+ http.ListenAndServe(":" + PORT, nil)
+}
