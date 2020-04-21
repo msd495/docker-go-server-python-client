71436414112ed9abbc70ac8977ea5bb3d5c0b14a
Ayush495 <msd495@gmail.com>
2020-04-21 22:10:49 +0530

    Add files via upload

--- client/client.py
+++ client/client.py
@@ -1,1 +1,10 @@
+import urllib.request
 
+fp = urllib.request.urlopen("http://localhost:3001/")
+
+encodedContent = fp.read()
+decodedContent = encodedContent.decode("utf8")
+
+print(decodedContent)
+
+fp.close()
